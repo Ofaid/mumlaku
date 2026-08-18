d3ebed5b2e0e0a75b05b3f8e246b29e6f7a2cedb
Ofaid <81507096+Ofaid@users.noreply.github.com>
2026-08-18 14:33:28 +0800

    Update MumlaActivity.java

--- app/src/main/java/se/lublin/mumla/app/MumlaActivity.java
+++ app/src/main/java/se/lublin/mumla/app/MumlaActivity.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-/*edit ke4 — Ganti data uji jadi data mikrofon asli */
+/*edit ke3 — Ganti data uji jadi data mikrofon asli */
 package se.lublin.mumla.app;
 
 import static java.util.Objects.requireNonNull;
