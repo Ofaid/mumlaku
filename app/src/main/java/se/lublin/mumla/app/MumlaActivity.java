c04d48fea6d20c0a8add6403c28e57e2ea1ba4fa
Ofaid <81507096+Ofaid@users.noreply.github.com>
2026-08-18 11:43:54 +0800

    Update MumlaActivity.java

--- app/src/main/java/se/lublin/mumla/app/MumlaActivity.java
+++ app/src/main/java/se/lublin/mumla/app/MumlaActivity.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+/*edit ke2*/
 package se.lublin.mumla.app;
 
 import static java.util.Objects.requireNonNull;
