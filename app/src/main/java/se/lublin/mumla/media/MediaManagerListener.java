a312a2f9394684e378ce44459336af8ebec15041
Ofaid <Ofaid@users.noreply.github.com>
2026-08-19 10:19:06 +0800

    Tambah visualizer garis tunggal

---
+++ app/src/main/java/se/lublin/mumla/media/MediaManagerListener.java
@@ -0,0 +1,11 @@
+package se.lublin.mumla.media;
+
+/**
+ * Date: 2020/11/24
+ * Author: Yang
+ * Describe: listener of media manager
+ */
+public interface MediaManagerListener {
+
+    void onPrepare();
+}
---
+++ app/src/main/java/se/lublin/mumla/visualize/VisualizeCallback.java
@@ -0,0 +1,12 @@
+package se.lublin.mumla.visualize;
+
+/**
+ * Date: 2020/11/24
+ * Author: Yang
+ * Describe:
+ */
+public interface VisualizeCallback {
+
+    void onFftDataCapture(float[] parseData);
+
+}
